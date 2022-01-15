acc4bdfbb2b40eab0422f64b301ea52c7bd3ba0e
coolabhays <abhaysp9955@gmail.com>
2022-01-15 14:12:50 +0530

    changed <CR> behaviour for pummenu

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -33,7 +33,7 @@ cmp.setup {
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
-      select = false,
+      select = true,
     },
 
     -- Tab mapping
