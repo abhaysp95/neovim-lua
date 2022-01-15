36888696213977ed702ab9efbbecd74fcc90502c
coolabhays <abhaysp9955@gmail.com>
2022-01-15 23:41:23 +0530

    file exp tree setting

---
+++ lua/plugins/nvim_tree.lua
@@ -0,0 +1,43 @@
+vim.g.nvim_tree_quit_on_open = 1
+vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_highlight_opened_files = 1
+vim.g.nvim_tree_disable_window_picker = 1
+vim.g.nvim_tree_respect_buf_cwd = 1
+vim.g.nvim_tree_width_allow_resize  = 1
+vim.g.nvim_tree_show_icons = {
+  git = 1,
+  folders = 1,
+  files = 1
+}
+vim.g.nvim_tree_window_picker_exclude = {
+  filetype = {
+    'packer',
+  },
+  buftype = {
+    'terminal',
+  }
+}
+
+vim.g.nvim_tree_icons = {
+	default = "‣ "
+}
+
+require('nvim-tree').setup {
+  open_on_setup = false,
+  update_cwd = true,
+  filters = {
+    dotfiles = true,
+    custom = { '.git', 'node_modules', '.cache', '.bin' },
+  },
+  git = {
+    enable = true,
+    ignore = true,
+  },
+  view = {
+    width = 28,
+    auto_resize = true
+  },
+}
+
+vim.cmd[[ highlight NvimTreeFolderIcon guibg=green ]]
