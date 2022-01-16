e2281c18865f897575dfd74b0edff850f87e750a
coolabhays <coolabhays@gmail.com>
2022-01-16 20:26:40 +0530

    verbose installation

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,8 +15,15 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  packer_bootstrap = fn.system{
+	  'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+	  install_path
+  },
+  print "Installing packer close and reopen nvim"
+  vim.cmd [[packadd packer.nvim]]
 end
+
+print "reached here"
 
 local status_ok, packer_config = pcall(require, 'packer')
 if not status_ok then
