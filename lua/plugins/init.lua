d3c0d985fa0eb584a9d723a14c52a7240a1151e6
coolabhays <abhaysp9955@gmail.com>
2022-01-20 20:07:02 +0530

    new themes and testing out lazy loading

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -56,12 +56,20 @@ packer_config.startup(function(use)
     -- ft = { 'sh', 'zsh', 'fish', 'c', 'cpp', 'cmake', 'go',
     --   'html', 'scss', 'css', 'javascript', 'javascriptreact',
     --   'lua', 'python', 'rust', 'typescript', 'typescriptreact' }
+    -- event = "BufRead",
   }
 
   -- LSP stuffs
-  use 'glepnir/lspsaga.nvim'
-  use 'onsails/lspkind-nvim'
-  use 'kosayoda/nvim-lightbulb'
+  use {
+    'glepnir/lspsaga.nvim',
+    -- event = "BufRead",
+    config = "require('plugins.lspsaga')"
+  }
+  use {
+    'onsails/lspkind-nvim',
+    -- event = "BufRead"
+  }
+  -- use 'kosayoda/nvim-lightbulb'
   use 'mfussenegger/nvim-jdtls'
   use 'mfussenegger/nvim-dap'
 
@@ -78,16 +86,30 @@ packer_config.startup(function(use)
       'hrsh7th/cmp-buffer',
       'saadparwaiz1/cmp_luasnip',
     },
+    -- event = "InsertEnter",
+    config = "require('plugins.nvim-cmp')",
   }
 
   -- autopairs
-  use 'windwp/nvim-autopairs'
+  use {
+    'windwp/nvim-autopairs',
+    config = "require('plugins.nvim-autopairs')",
+    after = "nvim-cmp"
+  }
 
   -- surround
-  use 'blackCauldron7/surround.nvim'
+  use {
+    'blackCauldron7/surround.nvim',
+    event = "BufEnter",
+    config = "require('plugins.surround')"
+  }
 
   -- commentary
-  use 'b3nj5m1n/kommentary'
+  use {
+    'b3nj5m1n/kommentary',
+    event = "BufEnter",
+    config = "require('plugins.kommentary')"
+}
 
   -- align text
   use 'godlygeek/tabular'
@@ -96,26 +118,38 @@ packer_config.startup(function(use)
   use {
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate',
-    -- requires = { 'p00f/nvim-ts-rainbow' }
+    -- requires = { 'p00f/nvim-ts-rainbow' },
+    event = "BufWinEnter",
+    config = "require('plugins.nvim-treesitter')"
   }
 
   -- tagviewer
-  use 'liuchengxu/vista.vim'
+  use {
+    'liuchengxu/vista.vim',
+    cmd = "Vista",
+    config = "require('plugins.vista')"
+  }
 
   -- indent line
-  use 'lukas-reineke/indent-blankline.nvim'
+  use {
+    'lukas-reineke/indent-blankline.nvim',
+    config = "require('plugins.indent-blankline')",
+    event = "BufRead"
+  }
 
   -- icons
   use 'kyazdani42/nvim-web-devicons'
 
-  -- start-page
-  use {
-    'goolord/alpha-nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' },
-    config = function ()
-      require'alpha'.setup(require'alpha.themes.dashboard'.opts)
-    end
-  }
+  -- startpage
+	-- use {
+  --   'glepnir/dashboard-nvim',
+  --   config = "require('plugins.dashboard')"
+  -- }
+  -- use {
+  --   'goolord/alpha-nvim',
+  --   requires = 'kyazdani42/nvim-web-devicons',
+  --   config = "require('plugins.alpha-nvim')",
+  -- }
 
   -- colorschemes
   use 'tanvirtin/monokai.nvim'
@@ -124,17 +158,26 @@ packer_config.startup(function(use)
   use 'cocopon/iceberg.vim'
   use 'whatyouhide/vim-gotham'
   use 'srcery-colors/srcery-vim'
+  use 'gruvbox-community/gruvbox'
   use 'sainnhe/gruvbox-material'
   use 'jacoborus/tender.vim'
   use 'jnurmine/Zenburn'
   use 'sainnhe/edge'
   use 'folke/tokyonight.nvim'
+  use 'tiagovla/tokyodark.nvim'
   use 'arzg/vim-colors-xcode'
-	use 'norcalli/nvim-colorizer.lua'
+	use {
+    'norcalli/nvim-colorizer.lua',
+    config = "require('plugins.colorizer')",
+    event = "BufRead",
+  }
   use 'yunlingz/equinusocio-material.vim'
   use 'ChristianChiarulli/nvcode-color-schemes.vim'
-  use { 'bluz71/vim-nightfly-guicolors', as = 'nightfly' }
+  use 'rebelot/kanagawa.nvim'
   use { 'rose-pine/neovim', as = 'rose-pine' }
+  use { 'catppuccin/nvim', as = 'catppuccin' }
+  use { 'bluz71/vim-moonfly-colors', as = 'moonfly' }
+  use { 'bluz71/vim-nightfly-guicolors', as = 'nightfly' }
 
   -- statusline
   use {
@@ -148,35 +191,45 @@ packer_config.startup(function(use)
     requires = {
       'nvim-lua/plenary.nvim',
       'nvim-lua/popup.nvim'
-    }
+    },
+    config = "require('plugins.telescope')",
   }
 
   -- telescope extension plugins
-  use 'nvim-telescope/telescope-media-files.nvim'
+  use {
+    'nvim-telescope/telescope-media-files.nvim',
+  }
 
   -- file explorer
   use {
     'kyazdani42/nvim-tree.lua',
     requires = { 'kyazdani42/nvim-web-devicons' },
+    cmd = "NvimTreeToggle",
+    config = "require('plugins.nvim_tree')"
   }
 
   -- git labels
   use {
     'lewis6991/gitsigns.nvim',
     requires = { 'nvim-lua/plenary.nvim' },
+    config = "require('plugins.gitsigns')"
   }
 
   -- floaterm
-  use 'voldikss/vim-floaterm'
+  use {
+    'voldikss/vim-floaterm',
+    config = "require('plugins.floaterm')",
+    cmd = "FloatermToggle"
+  }
 
   -- emmet-vim
   -- the plugin can be removed depending on how emmet-ls is working and
   -- how often I write html in markdown
-  use {
-    'mattn/emmet-vim',
-    opt = true,
-    ft = { 'html', 'css', 'markdown' }  -- can be updated later
-  }
+  -- use {
+  --   'mattn/emmet-vim',
+  --   opt = true,
+  --   ft = { 'html', 'css', 'markdown' }  -- can be updated later
+  -- }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
@@ -189,23 +242,7 @@ packer_config.startup(function(use)
   ------------------------------------------
 
   -- keybinds for installed plugins
-  require('plugins.nvim-cmp')
-  require('plugins.colorizer')
-  require('plugins.nvim_tree')
-  -- require('plugins.feline')  -- not a big fan of statuslines
-  require('plugins.nvim-treesitter')
-  require('plugins.gitsigns')
-  require('plugins.nvim-autopairs')
-  require('plugins.surround')
-  require('plugins.kommentary')
-  require('plugins.nvim-treesitter')
   require('plugins.telescope')
-  require('plugins.lspsaga')
-  require('plugins.alpha-nvim')
-  require('plugins.floaterm')
-  require('plugins.indent-blankline')
-  require('plugins.vista')
-  require('plugins.nvim-lightbulb')
   require('plugins.plugin_keybindings')
 
 end)
