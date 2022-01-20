40232f6f49c90b0809203ce82669a921af1e7e6e
coolabhays <abhaysp9955@gmail.com>
2022-01-20 20:04:35 +0530

    added sumneko_lua to loop

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -53,43 +53,57 @@ local on_attach = function(client, bufnr)
   local opts = { noremap=true, silent=true }
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', '<Leader>gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', '<Leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', 'gK', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<Leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<Leader>gK', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
   buf_set_keymap('n', '<Leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
   buf_set_keymap('n', '<Leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
   buf_set_keymap('n', '<Leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
   buf_set_keymap('n', '<Leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
   buf_set_keymap('n', '<Leader>gr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   buf_set_keymap('n', '<Leader>ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', 'gR', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<Leader>gR', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   buf_set_keymap('n', '<Leader>gl', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
   buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<Leader>lq', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
   buf_set_keymap('n', '<Leader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-
 end
 
---[[
 
-Language servers:
 
-Add your language server to `servers`
+-- Make runtime files discoverable to the server
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
 
-For language servers list see:
-https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+-- Set settings for language servers below
+--
+-- tsserver settings
+local ts_settings = function(client)
+  client.resolved_capabilities.document_formatting = false
+  ts_settings(client)
+end
 
-Bash --> bashls
-https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#bashls
+-- you can just put additional settings for other language servers here. e.g.,
 
---]]
+-- require 'some_lsp'.setup {
+--   default_config = {
+--     cmd = {};
+--     filetypes = {}
+--     rootdir= function()
+--       return <something>
+--     end;
+--     settings = {};
+--   };
+-- }
+
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'clangd', 'cmake', 'cssls', 'emmet_ls', 'gopls', 'html', 'pyright', 'rust_analyzer', 'tsserver' }
+local servers = { 'clangd', 'cmake', 'cssls', 'emmet_ls', 'gopls', 'html', 'pyright', 'sumneko_lua', 'rust_analyzer', 'tsserver' }
 
 --[[
 -- Installation(ls):
@@ -106,62 +120,51 @@ local servers = { 'clangd', 'cmake', 'cssls', 'emmet_ls', 'gopls', 'html', 'pyri
 -- tsserver:          npm install -g typescript typescript-language-server
 -- --]]
 
--- Set settings for language servers below
---
--- tsserver settings
-local ts_settings = function(client)
-  client.resolved_capabilities.document_formatting = false
-  ts_settings(client)
-end
 
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    ts_settings = ts_settings,
-    flags = {
-      debounce_text_changes = 150,
+  if lsp == 'sumneko_lua' then
+    nvim_lsp[lsp].setup{
+      cmd = { vim.fn.getenv 'HOME' .. '/.config/nvim/language-servers/lua-language-server/bin/lua-language-server' },
+      on_attach = on_attach,
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+            -- Setup your lua path
+            path = runtime_path,
+          },
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = { 'vim' },
+          },
+          workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file('', true),
+
+            -- below two line are fix (but takes too much resource)
+            -- maxPreload = 10000, -- Add this if missing or increase it
+            -- preloadFileSize = 10000, -- Add this if missing or increase it
+          },
+          -- Do not send telemetry data containing a randomized but unique identifier
+          telemetry = {
+            enable = false,
+          },
+        },
+      },
     }
-  }
+  else
+    nvim_lsp[lsp].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      ts_settings = ts_settings,
+      flags = {
+        debounce_text_changes = 150,
+      }
+    }
+  end
 end
-
-
--- Make runtime files discoverable to the server
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
-
-nvim_lsp.sumneko_lua.setup {
-  cmd = { vim.fn.getenv 'HOME' .. '/.config/nvim/language-servers/lua-language-server/bin/lua-language-server' },
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = runtime_path,
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file('', true),
-
-        -- below two line are fix (but takes too much resource)
-        maxPreload = 10000, -- Add this if missing or increase it
-        preloadFileSize = 10000, -- Add this if missing or increase it
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
 
 require('lspkind').init({
     -- enables text annotations
