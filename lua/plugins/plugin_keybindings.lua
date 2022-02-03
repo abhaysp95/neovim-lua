98eb6a82da2ad160951068c1cd3d6e1a1dd297d4
coolabhays <abhaysp9955@gmail.com>
2022-02-03 17:52:46 +0530

    change in keybinding of vim-tmux-navigator

--- lua/plugins/plugin_keybindings.lua
+++ lua/plugins/plugin_keybindings.lua
@@ -26,7 +26,7 @@ keybind('n' , '<C-h>'  , ':TmuxNavigateLeft<CR>'     , default_opts)
 keybind('n' , '<C-j>'  , ':TmuxNavigateDown<CR>'     , default_opts)
 keybind('n' , '<C-k>'  , ':TmuxNavigateUp<CR>'       , default_opts)
 keybind('n' , '<C-l>'  , ':TmuxNavigateRight<CR>'    , default_opts)
-keybind('n' , '<C-\\>' , ':TmuxNavigatePrevious<CR>' , default_opts)
+keybind('n' , '<C-->' , ':TmuxNavigatePrevious<CR>' , default_opts)
 
 ---------------
 -- nvim_tree --
