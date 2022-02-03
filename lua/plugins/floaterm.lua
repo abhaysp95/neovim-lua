d3d4bdfd72c36e96b168253f65cd40e7d907873d
coolabhays <abhaysp9955@gmail.com>
2022-02-03 17:51:04 +0530

    include some settings for floaterm

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -7,6 +7,24 @@
 --
 --  => lua/plugins/floaterm.lua
 
-vim.g.floaterm_position = 'bottom'
-vim.g.floaterm_width = 0.99
-vim.g.floaterm_height = 0.5
+vim.g.floaterm_title = 'cmd<$1|$2>'
+vim.g.floaterm_wintype = 'float'  -- split, vsplit, float
+vim.g.floaterm_position = 'center'
+vim.g.floaterm_width = 0.90
+vim.g.floaterm_height = 0.75
+vim.g.floaterm_borderchars = '─│─│┌┐┘└'  -- top, right, bottom, left, topleft, topright, botright, botleft
+vim.g.floaterm_autoclose = 0
+vim.g.floaterm_autohite = 1
+vim.g.floaterm_autoinsert = true
+
+
+-- other mapping options
+-- g:floaterm_keymap_new
+-- g:floaterm_keymap_prev
+-- g:floaterm_keymap_next
+-- g:floaterm_keymap_first
+-- g:floaterm_keymap_last
+-- g:floaterm_keymap_hide
+-- g:floaterm_keymap_show
+-- g:floaterm_keymap_kill
+-- g:floaterm_keymap_toggle
