a8bf630a7a403b98656856ab80721cbc5fa5843e
coolabhays <abhaysp9955@gmail.com>
2022-02-05 17:25:23 +0530

    update floaterm preference

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -9,9 +9,9 @@
 
 vim.g.floaterm_title = 'cmd<$1|$2>'
 vim.g.floaterm_wintype = 'float'  -- split, vsplit, float
-vim.g.floaterm_position = 'center'
+vim.g.floaterm_position = 'bottom'
 vim.g.floaterm_width = 0.95
-vim.g.floaterm_height = 0.75
+vim.g.floaterm_height = 0.60
 vim.g.floaterm_borderchars = '─│─│┌┐┘└'  -- top, right, bottom, left, topleft, topright, botright, botleft
 vim.g.floaterm_autoclose = 0
 vim.g.floaterm_autohite = 1
