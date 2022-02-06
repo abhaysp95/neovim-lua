15197bcdfce026c21b179db6eb0829d5a02b4bb1
coolabhays <abhaysp9955@gmail.com>
2022-02-06 17:19:45 +0530

    add configuration files for github, gruvbox, material and nightfox colorscheme

---
+++ lua/colors/material.lua
@@ -0,0 +1,4 @@
+-- five styles to choose from: darker, lighter, oceanic, palenight, deep ocean
+vim.g.material_style = "deep ocean"
+
+-- for more configuration, visit: https://github.com/marko-cerovac/material.nvim
