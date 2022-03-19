42c4254278e31fd966449bb8709d47cb296c0b71
coolabhays <abhaysp9955@gmail.com>
2022-03-19 13:03:02 +0530

    update config by removing/updating deprecated options

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -7,45 +7,57 @@
 --
 --  => lua/plugins/nvim_tree.lua
 
-vim.g.nvim_tree_quit_on_open = 0
+vim.g.hijack_netrw = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_highlight_opened_files = 1
-vim.g.nvim_tree_disable_window_picker = 1
 vim.g.nvim_tree_respect_buf_cwd = 1
 vim.g.nvim_tree_width_allow_resize  = 1
 vim.g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
-  files = 1
-}
-vim.g.nvim_tree_window_picker_exclude = {
-  filetype = {
-    'packer',
-  },
-  buftype = {
-    'terminal',
-  }
+	git = 1,
+	folders = 1,
+	files = 1
 }
 
 vim.g.nvim_tree_icons = {
 	default = "‣ "
 }
 
-require('nvim-tree').setup {
-  open_on_setup = false,
-  update_cwd = true,
-  auto_close = true,  -- I'll update this after testing
-  filters = {
-    dotfiles = true,
-    custom = { '.git', 'node_modules', '.cache', '.bin' },
-  },
-  git = {
-    enable = true,
-    ignore = true,
-  },
-  view = {
-    width = 28,
-    auto_resize = true
-  },
+local status_ok, nvim_tree_config = pcall(require, "nvim-tree")
+if not status_ok then
+	return
+end
+
+nvim_tree_config.setup {
+	open_on_setup = false,
+	update_cwd = true,
+	auto_close = true,  -- I'll update this after testing
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = true,
+				exclude = {
+					filetype = {
+						'packer',
+					},
+					buftype = {
+						'terminal',
+					},
+				},
+			},
+			quit_on_open = false,
+		},
+	},
+	filters = {
+		dotfiles = true,
+		custom = { '.git', 'node_modules', '.cache', '.bin' },
+	},
+	git = {
+		enable = true,
+		ignore = true,
+	},
+	view = {
+		width = 28,
+		auto_resize = true
+	},
 }
