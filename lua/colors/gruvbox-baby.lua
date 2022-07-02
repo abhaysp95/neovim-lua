57246c17f4bdf911a21ad05b89cf5c625c0ab52d
coolabhays <abhaysp9955@gmail.com>
2022-07-02 08:28:49 +0530

    using gruvbox-baby instead of gruvbox

---
+++ lua/colors/gruvbox-baby.lua
@@ -0,0 +1,7 @@
+vim.g.gruvbox_baby_background_color = "dark"
+vim.g.gruvbox_baby_transparent_mode = false
+vim.g.gruvbox_baby_comment_style =  "italic"
+vim.g.gruvbox_baby_keyword_style = "NONE"
+vim.g.gruvbox_baby_variable_style = "NONE"
+vim.g.gruvbox_baby_telescope_theme = 1
+-- vim.g.gruvbox_baby_highlights = {Normal = {fg = "#d4be98", bg = "NONE", style="NONE"}}
