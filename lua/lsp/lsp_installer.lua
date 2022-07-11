bbe60a1c93c5be5e27bf58b1517d65afffc0e11a
coolabhays <abhaysp9955@gmail.com>
2022-07-11 09:37:10 +0530

    feat: lsp install settings for different servers

---
+++ lua/lsp/lsp_installer.lua
@@ -0,0 +1,52 @@
+local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not status_ok then
+	return
+end
+
+local servers = {
+	"clangd",
+	"cmake",
+	"cssls",
+	"emmet_ls",
+	"gopls",
+	"html",
+	"pyright",
+	"solc",
+	"jdtls",
+	"sumneko_lua",
+	"rust_analyzer",
+	"tsserver"
+}
+
+lsp_installer.setup()
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+	return
+end
+
+local opts = {}
+for _, server in pairs(servers) do
+	opts = {
+		on_attach = require("lsp.handlers").on_attach,
+		capabilities = require("lsp.handlers").capabilities,
+	}
+
+	--[[ if server == "sumneko_lua" then
+		local sumneko_opts = require "lsp.settings.sumneko_lua"
+		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+	end
+
+	if server == "pyright" then
+		local pyright_opts = require "lsp.settings.pyright"
+		opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+	end ]]
+
+	if server == "jdtls" then
+		goto continue
+	end
+
+	lspconfig[server].setup(opts)
+
+	::continue::
+end
